cc39c2f422f3026cc3dbbccea343b19fc709d572
Christina Kiesz <ckiesz@mesosphere.com>
2019-08-07 20:45:44 -0700

    Set latest t0 1.14 for alpha

--- index.js
+++ index.js
@@ -111,7 +111,7 @@ MS.metadata({
     copyright: `&copy; ${currentYear} D2iQ, Inc. All rights reserved.`,
     env: process.env.NODE_ENV,
     gitBranch: GIT_BRANCH,
-    dcosDocsLatest: '1.13',
+    dcosDocsLatest: '1.14',
     dcosCNDocsLatest: '1.12',
     konvoyDocsLatest: 'latest',
     kommanderDocsLatest: '0.1',
